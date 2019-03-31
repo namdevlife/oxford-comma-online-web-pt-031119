796853a9f56be9838d236a8aa29febc9ef3847e9
NAM NGUYEN <namdevlife@gmail.com>
2019-03-31 23:03:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
-array = []
-
 def oxford_comma(array)
-    array.join(",")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
-
-puts oxford_comma
